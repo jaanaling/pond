121bbf56bba3917ee74572772fef35df5b4123ec
«Vennylost» <anastasiabarsukova2004@gmail.com>
2024-11-25 18:43:28 +0300

    first grey

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'dart:async';
+
+import 'package:core_logic/core_logic.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:pond_care/routes/route_value.dart';
 import 'package:pond_care/src/core/dependency_injection.dart';
 import 'package:pond_care/src/feature/app/presentation/app_root.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:core_amplitude/core_amplitude.dart';
 
 late bool isCreateScreenLoaded;
 late bool isEditScreenLoaded;
@@ -10,21 +15,56 @@ late bool isOverviewScreenLoaded;
 late bool isRecommendationScreenLoaded;
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  setupDependencyInjection();
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    // ... your code here
 
-  final prefs = await SharedPreferences.getInstance();
-  isCreateScreenLoaded = prefs.getBool('createScreen') ?? false;
-  isEditScreenLoaded = prefs.getBool('editScreen') ?? false;
-  isOverviewScreenLoaded = prefs.getBool('overviewScreen') ?? false;
-  isRecommendationScreenLoaded = prefs.getBool('recommendationScreen') ?? false;
+    FlutterError.onError = (FlutterErrorDetails details) {
+      _handleFlutterError(details);
+    };
+    setupDependencyInjection();
 
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
+    await InitializationUtil.coreInit(
+      domain: 'pondcarea.com',
+      amplitudeKey: 'ab7fc781165b2ee1db9e5abe0e9412eb',
+      appsflyerDevKey: 'iUCiBLKckw4HYFfLnLZ6M6',
+      appId: 'com.pioneerbloom.pondcare',
+      iosAppId: '6738591800',
+      initialRoute: RouteValue.home.path,
+    );
 
-  runApp(
-    const AppRoot(),
+    final prefs = await SharedPreferences.getInstance();
+    isCreateScreenLoaded = prefs.getBool('createScreen') ?? false;
+    isEditScreenLoaded = prefs.getBool('editScreen') ?? false;
+    isOverviewScreenLoaded = prefs.getBool('overviewScreen') ?? false;
+    isRecommendationScreenLoaded =
+        prefs.getBool('recommendationScreen') ?? false;
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    runApp(
+      const AppRoot(),
+    );
+  }, (Object error, StackTrace stackTrace) {
+    _handleAsyncError(error, stackTrace);
+  });
+}
+
+void _handleFlutterError(FlutterErrorDetails details) {
+  AmplitudeUtil.logFailure(
+    details.exception is Exception ? Failure.exception : Failure.error,
+    details.exception.toString(),
+    details.stack,
+  );
+}
+
+void _handleAsyncError(Object error, StackTrace stackTrace) {
+  AmplitudeUtil.logFailure(
+    error is Exception ? Failure.exception : Failure.error,
+    error.toString(),
+    stackTrace,
   );
 }
--- lib/src/feature/splash/presentation/screens/splash_screen.dart
+++ lib/src/feature/splash/presentation/screens/splash_screen.dart
@@ -1,67 +1,49 @@
-import 'package:advertising_id/advertising_id.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:core_logic/core_logic.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
 import 'package:go_router/go_router.dart';
+import 'package:pond_care/src/core/utils/app_icon.dart';
+import 'package:pond_care/src/core/utils/icon_provider.dart';
 
-import '../../../../../routes/route_value.dart';
-import '../../../../core/utils/app_icon.dart';
-import '../../../../core/utils/icon_provider.dart';
-
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
-  @override
-  void initState() {
-    super.initState();
-    startLoading(context);
-  }
-
-  Future<void> startLoading(BuildContext context) async {
-    await Future.delayed(const Duration(milliseconds: 1000));
-
-    final adId = await AdvertisingId.id(true);
-    context.go(RouteValue.home.path);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Positioned.fill(
-          child: AppIcon(
-            asset: IconProvider.splash.buildImageUrl(),
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
+    return BlocProvider(
+      create: (context) => InitializationCubit()..initialize(),
+      child: BlocListener<InitializationCubit, InitializationState>(
+        listener: (context, state) {
+          if (state is InitializedState) {
+            context.go(state.startRoute);
+          }
+        },
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Positioned.fill(
+              child: AppIcon(
+                asset: IconProvider.splash.buildImageUrl(),
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned(
+              top: height * 0.122,
+              child: AppIcon(asset: IconProvider.logo.buildImageUrl()),
+            ),
+            Positioned(
+              bottom: height * 0.122,
+              child: const SpinKitFadingCircle(
+                color: Colors.white,
+                size: 98,
+              ),
+            ),
+          ],
         ),
-        Positioned(
-          top: height * 0.122,
-          child: AppIcon(asset: IconProvider.logo.buildImageUrl()),
-        ),
-        Positioned(
-          bottom: height * 0.122,
-          child: const SpinKitFadingCircle(
-            color: Colors.white,
-            size: 98,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
